e1f3649f4bf1c20d7e82e8057aa525996786b715
b7sh <mohammedabdelbasit500@gmail.com>
2023-11-28 03:57:31 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for index1 in range(8):
     for index2 in range(index1 + 1, 10):
-        print(f"{index1:d}{index2:d}", end=", ")
-print(f"{index1 + 1:d}{index2:d}")
+        print("{:d}{:d}".format(index1, index2), end=", ")
+print("{:d}{:d}".format(index1 + 1, index2))
