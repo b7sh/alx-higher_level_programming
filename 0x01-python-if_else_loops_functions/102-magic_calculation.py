3f8e3b3276b872f0ff0d7cf74cf5c26cc58d0cf9
b7sh <mohammedabdelbasit500@gmail.com>
2023-11-28 10:28:59 -0800

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    elif a > b:
+    if a > b and a <= 2:
         return a + b
-    else:
+    elif a < b:
+        return c
+    elif a > 2 and a > b:
         return (a * b) - c
