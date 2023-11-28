e12ad794253bc90c09defe47afa8da996dbcffe4
b7sh <mohammedabdelbasit500@gmail.com>
2023-11-28 03:51:11 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,4 @@
 for x in range(100):
     if x != 99:
         print("{:02}".format(x), end=", ")
-    else:
-        print("{:02}".format(x), end="")
-        print()
+    print("{:02}".format(x), end="")
