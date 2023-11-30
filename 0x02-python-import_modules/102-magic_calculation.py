7fd20225d291e7ba97de1cee3eac9dbdca65ddd2
b7sh <mohammedabdelbasit500@gmail.com>
2023-11-30 04:46:39 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
     if a > b:
         result = add(a, b)
     else:
