85c779e8cbc5fd815be2f46b99b5a3454f766789
b7sh <mohammedabdelbasit500@gmail.com>
2023-11-30 01:48:06 -0800

    modified 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import hidden_4
     show = dir(hidden_4)
-    l = len(show)
-    for item in range(l):
+    lenght = len(show)
+    for item in range(lenght):
         if show[item][:2] == '__':
             continue
         else:
