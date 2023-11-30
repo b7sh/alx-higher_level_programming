31bbce9ddfdfe0ea6900d2d7d70302c156e545d1
b7sh <mohammedabdelbasit500@gmail.com>
2023-11-30 02:10:53 -0800

    last modified on 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
         print("{} arguments.".format(argc - 1))
     else:
         if argc == 2:
-            print("{} arguments:".format(argc - 1))
+            print("{} argument:".format(argc - 1))
         else:
-            print("{} arguments:".format(argc -1))
+            print("{} arguments:".format(argc - 1))
         for n in range(1, argc):
             print("{}: {}".format(n, argv[n]))
