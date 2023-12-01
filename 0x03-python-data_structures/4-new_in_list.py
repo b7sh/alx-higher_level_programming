9ba9920ea5357aa5e9a3981dfe8194718af0168a
b7sh <mohammedabdelbasit500@gmail.com>
2023-12-01 03:31:52 -0800

    modified the old files and add 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element)
+def new_in_list(my_list, idx, element):
+    new = my_list.copy()
+    if idx < 0:
+        return new
+    ln = len(new)
+    if idx > ln:
+        return new
+    new[idx] = element
+    return new
