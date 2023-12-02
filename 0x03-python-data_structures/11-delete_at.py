7a1defedba9d16c4380204d3035dde8774b14246
b7sh <mohammedabdelbasit500@gmail.com>
2023-12-02 04:41:08 -0800

    modified 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     ln = len(my_list)
-    if idx > ln:
+    if idx > ln - 1:
         return my_list
     else:
         my_list.remove(idx + 1)
