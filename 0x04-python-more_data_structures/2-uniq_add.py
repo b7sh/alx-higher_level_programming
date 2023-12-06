0fbd1d33062c3c51e5e07546a0f9f36f6de8b609
b7sh <mohammedabdelbasit500@gmail.com>
2023-12-06 07:07:05 -0800

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     result = 0
-    for item in range(len(my_list)):
+    for item in my_list:
         if my_list.count(item) < 1:
             continue
         else:
