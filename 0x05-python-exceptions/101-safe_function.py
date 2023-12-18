b1c8d2695b2b63bada23d6628a5e7e34d336edaf
b7sh <mohammedabdelbasit500@gmail.com>
2023-12-18 12:49:44 -0800

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        exe = fct(*args)
+        return exe
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
