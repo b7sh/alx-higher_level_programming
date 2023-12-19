740a2dfce2396aed74fd38b2d0cf90fc7c3bab49
b7sh <mohammedabdelbasit500@gmail.com>
2023-12-19 14:37:36 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,12 +49,13 @@ class Square:
         """print in stdout the square with the character #"""
         if self.__size == 0:
             print("")
-            return
-        for i in range(self.__position[1]):
+
+        else:
+            for i in range(self.__position[1]):
                 print("")
-        for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print(" ", end="")
-            for k in range(self.__size):
-                print("#", end="")
-            print("")
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print("")
