9c7340f120126e8fcbcde9b171b45d0d37d04fc6
b7sh <mohammedabdelbasit500@gmail.com>
2023-12-19 10:30:00 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""defines a square"""
+
+
+class Square:
+    def __init__(self, size):
+        """new square
+
+        size: the size of the squae"""
+        self.__size = size
