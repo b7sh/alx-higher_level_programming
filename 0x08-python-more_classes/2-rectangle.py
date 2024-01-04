858fab8cb804bf8cd488a493baf5cb9d336010b0
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-04 13:09:27 -0800

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,10 +42,10 @@ class Rectangle():
 
     def area(self):
         """the area value"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """the perimeter value"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return ((self.__width * 2) + (self.__height * 2))
+        return ((self.__width + self.__height) * 2)
