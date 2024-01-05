db1b57da8e78092a741e8c462c741be2cf96ceea
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-05 07:31:40 -0800

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class LockedClass with no class or object attribute"""
+
+
+class LockedClass():
+    """prevents the user from dynamically
+    creating new instance attributes"""
+    __slots__ = "first_name"
