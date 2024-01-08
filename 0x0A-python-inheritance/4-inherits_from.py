7e59e20b58357cd546179f03abb858e285cc0d12
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-08 05:21:51 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,7 @@ an instance of a class that inherited
 
 
 def inherits_from(obj, a_class):
+    """check if it's an instance from specified class"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
