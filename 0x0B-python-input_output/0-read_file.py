e91ed04b7a71db9043757fc7a48ec838ad35bfec
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-09 03:17:44 -0800

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that reads a text file and print it to the stdout"""
+
+
+def read_file(filename=""):
+    with open(filename, 'r', encoding='UTF-8') as rf:
+        for line in rf:
+            print(line, end='')
