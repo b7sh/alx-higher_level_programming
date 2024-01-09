a13c85557528158e9c77a0b7f013f4b03cb5a4c1
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-09 03:38:53 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ a function that writes a string to a
-text file (UTF8) and returns the
-number of characters written
+text file and returns the number of characters written
 """
 
 def write_file(filename="", text=""):
-    """return the number of characters written to the file"""
+    """return the number of characters written to the file
+    args:
+        filename (str): the name of the file
+        text (str): the text whoes written to the file
+        """
     with open(filename, 'w', encoding='UTF-8') as wf:
         lenght = wf.wite(text)
         return lenght
