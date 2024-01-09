8599dfdc2ec33d20a43c1f6890003cf9e288761c
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-09 04:05:14 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/sur/bin/python3
+"""a function that returns the JSON representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """return the JSON representation
+    args:
+        my_obj (str): the string
+        """
+    return json.dumps(my_obj)
