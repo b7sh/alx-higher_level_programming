16ac43fa18887520ea0ae57bf85c38158321ed34
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-09 03:36:04 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that writes a string to a
+text file (UTF8) and returns the
+number of characters written
+"""
+
+def write_file(filename="", text=""):
+    """return the number of characters written to the file"""
+    with open(filename, 'w', encoding='UTF-8') as wf:
+        lenght = wf.wite(text)
+        return lenght
