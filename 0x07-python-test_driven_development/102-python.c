ac512b43f5550708260901c939fa5423e3d3e040
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-10 03:55:31 -0800

    102-

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -18,7 +18,7 @@ void print_python_string(PyObject *p)
 			return;
 	}
 
-	len = ((PyASCIIobject *(p))->len;
+	len = (PyASCIIObject *(p))->len;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf(" type: compact ascii\n");
