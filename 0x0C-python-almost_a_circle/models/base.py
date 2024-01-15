779b57730f1e09838afd58730773e0380bc541bd
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-15 08:00:55 -0800

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,12 +35,11 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation
-        of list_objs to a file
-        """
+        """save the JSON file object to file"""
         if list_objs is not None:
             list_objs = [line.to_dictionary() for line in list_objs]
-            with open("{}.json".format(cls.__name__), 'w', encoding="utf-8") as f:
+            with open("{}.json".format(
+                    cls.__name__), 'w', encoding="utf-8") as f:
                 f.write(cls.to_json_string(list_objs))
 
     @staticmethod
