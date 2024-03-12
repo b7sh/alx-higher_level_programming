86eff7011bc7aaf63c0b09c238f1a605b58ad44f
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-12 09:12:42 -0700

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+class Square extends Square1 {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let square = '';
+      for (let j = 0; j < this.width; j++) {
+        square += c;
+      }
+      console.log(square);
+    }
+  }
+}
+module.exports = Square;
