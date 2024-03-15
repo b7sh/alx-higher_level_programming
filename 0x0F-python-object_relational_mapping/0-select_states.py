4d1d3461ab37881b97ed88c72411d73a4dad1245
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-15 11:50:10 -0700

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+connection = MySQLdb.connect(host='localhost',
+                             user=sys.argv[1],
+                             password=sys.argv[2],
+                             db=sys.argv[3],
+                             port=3306)
+cursor = connection.cursor()
+cursor.execute('SELECT * FROM states')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+connection.close()
