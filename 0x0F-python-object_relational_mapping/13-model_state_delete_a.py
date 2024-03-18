610913003a015a5789c75607ae1ba5c5e8fa8a76
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-17 17:33:05 -0700

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for new_states in session.query(State).filter(State.name.like('%a%')).first()
+    for new_states in session.query(State).filter(State.name.like('%a%'))
         session.delete(new_states)
     session.commit()
