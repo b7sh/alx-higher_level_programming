87e8aa2250da640a23f3e399c58dcad917ccbf4b
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-30 21:19:55 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,7 +5,7 @@
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
 
-    if list_of_integers == []:
+    if list_of_integers is None or list_of_integers == []:
         return None
     low = 0
     heigh = len(list_of_integers)
