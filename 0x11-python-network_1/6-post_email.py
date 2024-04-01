2ee2d3abeeb2c7a461d2eaa7345f22ab988ca8c1
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 22:02:12 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+    takes in a URL and an email address,
+    sends a POST request to the passed URL
+    with the email as a parameter,
+    and finally displays the body of the response.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    req = sys.argv[1]
+    data = {"email": sys.argv[2]}
+    r = requests.post(req, params=data)
+    print("{}".format(r.text))
