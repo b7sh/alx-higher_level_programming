c637a54a1799ffc215987d95b8dcebb253cf91e4
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 20:44:51 -0700

    the first task done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" fetche https://alx-intranet.hbtn.io/stat """
+import urllib.request
+
+
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    html = response.read()
+print("Body response:")
+print("\t - type: {}".format(type(html)))
+print("\t - content: {}".format(html))
+print("\t - utf8 content: {}".format(html.decode("utf-8")))
