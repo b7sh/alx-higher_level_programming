60428c667fed87318bb2385fac95c519799ca9cd
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 22:09:38 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    takes in a URL, sends a request
+    to the URL and displays the body
+    of the response.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    req = sys.argv[1]
+    r = requests.get(req)
+    if r.status_code >= 400:
+        print("Error code:".format(r.status_code))
+    else:
+        print(r.text)
