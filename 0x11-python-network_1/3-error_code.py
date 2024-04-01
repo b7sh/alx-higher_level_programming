a68cb4fea00052afb8f7a08cacc2e41e535ab507
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 21:35:25 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    take in a URL, sends a request to the URL
+    and displays the body of the response (decoded in utf-8)
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.error
+
+    req = sys.argv[1]
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: ", e.code)
