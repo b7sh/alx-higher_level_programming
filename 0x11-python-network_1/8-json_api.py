8ed666681d654f26f5a9ccba85088afd37fd82bb
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 22:21:06 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+    take in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with
+    the letter as a parameter.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    value = {"q": letter}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=value)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
