d0ff75486638f3e7bf2d51220904590fec67db56
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 22:10:52 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     req = sys.argv[1]
     r = requests.get(req)
     if r.status_code >= 400:
-        print("Error code:".format(r.status_code))
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
