43d340b52ba6e3803c1eb64504857378328bc7b8
b7sh <mohammedabdelbasit500@gmail.com>
2024-03-31 21:25:09 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    take in a URL and an email, sends a POST request
+    to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
